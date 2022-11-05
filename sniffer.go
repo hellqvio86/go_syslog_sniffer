b9a97c683654247e10274d2ac9f2c0d4d6c6e519
Olof Hellqvist <olof.hellqvist@gmail.com>
2022-11-05 13:56:53 +0100

    Adding json support

--- sniffer.go
+++ sniffer.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
+	"regexp"
 	"sort"
 	"strconv"
 	"time"
@@ -14,20 +17,57 @@ import (
 	"github.com/google/gopacket/pcap"
 )
 
-var ipAddrs = make(map[string]uint64)
+type syslogCapture struct {
+	Payload   string `json:"payload"`
+	Unix_time int64  `json:"unix_time"`
+}
+type sysloghost struct {
+	IPv4Package  uint64        `json:"ipv4Package"`
+	IPv6Package  uint64        `json:"ipv6Package"`
+	UDPDatagrams uint64        `json:"udpDatagrams"`
+	TCPPackages  uint64        `json:"tcpPackages"`
+	SampleEvent  syslogCapture `json:"sampleEvent"`
+}
+
+var ipAddrs = make(map[string]sysloghost)
 
 const (
 	// The same default as tcpdump.
 	defaultSnapLen = 262144
 )
 
+func add_payload_to_map(srcIp string, pkt gopacket.Packet) {
+	var syslogMsgRegexp = regexp.MustCompile(`.*<\d+>.*$`)
+	syslog_host := ipAddrs[srcIp]
+
+	if syslog_host.SampleEvent.Payload != "" {
+		// Seen before
+		return
+	}
+
+	applicationLayer := pkt.ApplicationLayer()
+	if applicationLayer != nil {
+		payload := string(applicationLayer.Payload())
+		//log.Print("Source ip: ", srcIp, " Payload: ", payload)
+
+		if syslogMsgRegexp.MatchString(payload) {
+			new_syslog_msg := syslogCapture{payload, time.Now().Unix()}
+			syslog_host.SampleEvent = new_syslog_msg
+
+			ipAddrs[srcIp] = syslog_host
+		}
+	}
+}
+
 func analyse_packet(pkt gopacket.Packet) {
 	// Let's see if the packet is IP (even though the ether type told us)
 	ipLayer := pkt.Layer(layers.LayerTypeIPv4)
+	scrIp := ""
+
 	if ipLayer != nil {
 		//fmt.Println("IPv4 layer detected.")
 		ip, _ := ipLayer.(*layers.IPv4)
-		scrIp := ip.SrcIP.String()
+		scrIp = ip.SrcIP.String()
 
 		// IP layer variables:
 		// Version (Either 4 or 6)
@@ -38,14 +78,16 @@ func analyse_packet(pkt gopacket.Packet) {
 		//fmt.Println("Protocol: ", ip.Protocol)
 		//fmt.Println()
 
-		ipAddrs[scrIp] = ipAddrs[scrIp] + 1
+		sysloghost := ipAddrs[scrIp]
+		sysloghost.IPv4Package = sysloghost.IPv4Package + 1
+		ipAddrs[scrIp] = sysloghost
 	}
 
 	ip6Layer := pkt.Layer(layers.LayerTypeIPv6)
 	if ip6Layer != nil {
 		//fmt.Println("IPv6 layer detected.")
 		ip, _ := ip6Layer.(*layers.IPv6)
-		scrIp := ip.SrcIP.String()
+		scrIp = ip.SrcIP.String()
 
 		// IP layer variables:
 		// Version (Either 4 or 6)
@@ -55,8 +97,26 @@ func analyse_packet(pkt gopacket.Packet) {
 		//fmt.Printf("From %s to %s\n", ip.SrcIP, ip.DstIP)
 		//fmt.Println()
 
-		ipAddrs[scrIp] = ipAddrs[scrIp] + 1
+		sysloghost := ipAddrs[scrIp]
+		sysloghost.IPv6Package = sysloghost.IPv6Package + 1
+		ipAddrs[scrIp] = sysloghost
 	}
+
+	tcpLayer := pkt.Layer(layers.LayerTypeTCP)
+	if tcpLayer != nil {
+		sysloghost := ipAddrs[scrIp]
+		sysloghost.TCPPackages = sysloghost.TCPPackages + 1
+		ipAddrs[scrIp] = sysloghost
+	}
+
+	udpLayer := pkt.Layer(layers.LayerTypeUDP)
+	if udpLayer != nil {
+		sysloghost := ipAddrs[scrIp]
+		sysloghost.UDPDatagrams = sysloghost.UDPDatagrams + 1
+		ipAddrs[scrIp] = sysloghost
+	}
+
+	add_payload_to_map(scrIp, pkt)
 }
 
 func sniff(intf string, bpffiler string, duration time.Duration) {
@@ -116,11 +176,36 @@ func main() {
 	}
 	sort.Strings(keys)
 
-	fmt.Println("IP,count")
+	fmt.Println("Sample events:")
+	for _, key := range keys {
+		fmt.Print("IP: ")
+		fmt.Print(key)
+		fmt.Print(" Syslog message: ")
+		fmt.Println(ipAddrs[key].SampleEvent.Payload)
+	}
+
+	jsonByte, err := json.Marshal(ipAddrs)
+	if err != nil {
+		log.Println("Failed with json Mashal", err)
+	}
+
+	_ = ioutil.WriteFile("syslog_flow.json", jsonByte, 0644)
+
+	//fmt.Println("\n")
+	//look_for_invalid_timestamps(keys)
+	//fmt.Println("\n")
+
+	fmt.Println("IP,ipv4Package,ipv6Package,udpDatagrams,tcpPackages")
 	for _, key := range keys {
 		fmt.Print(key)
 		fmt.Print(",")
-		fmt.Print(ipAddrs[key])
+		fmt.Print(ipAddrs[key].IPv4Package)
+		fmt.Print(",")
+		fmt.Print(ipAddrs[key].IPv6Package)
+		fmt.Print(",")
+		fmt.Print(ipAddrs[key].UDPDatagrams)
+		fmt.Print(",")
+		fmt.Print(ipAddrs[key].TCPPackages)
 		fmt.Println("")
 	}
 }
